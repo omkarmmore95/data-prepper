7572b146755795b26049fe939b87fa50ad273231
omkarmmore95 <omkar.m.more95@gmail.com>
2023-06-13 09:55:28 +0530

    -Support for Sink Codecs Signed-off-by: omkarmmore95 <omkar.m.more95@gmail.com>

--- data-prepper-plugins/s3-sink/src/main/java/org/opensearch/dataprepper/plugins/sink/ThresholdCheck.java
+++ data-prepper-plugins/s3-sink/src/main/java/org/opensearch/dataprepper/plugins/sink/ThresholdCheck.java
@@ -8,8 +8,6 @@ package org.opensearch.dataprepper.plugins.sink;
 import org.opensearch.dataprepper.model.types.ByteCount;
 import org.opensearch.dataprepper.plugins.sink.accumulator.Buffer;
 
-import java.io.ByteArrayOutputStream;
-
 /**
  * Check threshold limits.
  */
@@ -26,21 +24,14 @@ public class ThresholdCheck {
      * @param maxCollectionDuration maximum event collection duration provided by user as threshold.
      * @return boolean value whether the threshold are met.
      */
-    public static boolean checkThresholdExceed(final Buffer currentBuffer, final int maxEvents, final ByteCount maxBytes, final long maxCollectionDuration
-            , final ByteArrayOutputStream byteArrayOutputStream, final int eventCount) {
+    public static boolean checkThresholdExceed(final Buffer currentBuffer, final int maxEvents, final ByteCount maxBytes, final long maxCollectionDuration) {
         if (maxEvents > 0) {
-            /*return currentBuffer.getEventCount() + 1 > maxEvents ||
+            return currentBuffer.getEventCount() + 1 > maxEvents ||
                     currentBuffer.getDuration() > maxCollectionDuration ||
-                    currentBuffer.getSize() > maxBytes.getBytes();*/
-            return eventCount + 1 > maxEvents ||
-                    currentBuffer.getDuration() > maxCollectionDuration ||
-                    byteArrayOutputStream.size() > maxBytes.getBytes();
+                    currentBuffer.getSize() > maxBytes.getBytes();
         } else {
-            /*return currentBuffer.getDuration() > maxCollectionDuration ||
-                    currentBuffer.getSize() > maxBytes.getBytes();*/
             return currentBuffer.getDuration() > maxCollectionDuration ||
-                    byteArrayOutputStream.size() > maxBytes.getBytes();
-
+                    currentBuffer.getSize() > maxBytes.getBytes();
         }
     }
 }
